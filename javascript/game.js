f63acdf409afe2b6a225234b0b4a1deae464078c
Tim Gangl <tim.gangl30@gmail.com>
2019-10-10 17:06:08 -0700

    just need to make user score reset to 0

--- javascript/game.js
+++ javascript/game.js
@@ -1,124 +1,110 @@
-//element var
-var fluor = $("#fluorite");
-var labra = $("#labradorite");
-var opal = $("#opal");
-var ruby = $("#ruby");
-var targetNum = $("#target-num");
-var wins = $("#wins");
-var losses = $("#losses");
-var userScore = $("#user-score");
+$(document).ready(function () {
 
-//button values
-var numTarget = 0;
-var winsTally = 0;
-var lossesTally = 0;
-var scoreUser = 0;
+    //element var
+    var fluor = $("#fluorite");
+    var labra = $("#labradorite");
+    var opal = $("#opal");
+    var ruby = $("#ruby");
+    var targetNum = $("#target-num");
+    var wins = $("#wins");
+    var losses = $("#losses");
+    var userScore = $("#user-score");
 
-var fluorValue = 0;
-var labraValue = 0;
-var opalValue = 0;
-var rubyValue = 0;
+    //button values
+    var numTarget = 0;
+    var winsTally = 0;
+    var lossesTally = 0;
+    var scoreUser = 0;
 
+    var fluorValue = 0;
+    var labraValue = 0;
+    var opalValue = 0;
+    var rubyValue = 0;
 
-
-
-//functions-----------
-
-//  generate random number
-function generateTargetNum() {
-    var result = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    numTarget = result;
-    targetNum.text(result);
-}
-generateTargetNum();
-
-//display random number on screen
-targetNum.text(numTarget);
-
-// assign random values to crystals
-function generateCrystalValue() {
-    return Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-
-};
-
-fluorValue = generateCrystalValue();
-labraValue = generateCrystalValue();
-opalValue = generateCrystalValue();
-rubyValue = generateCrystalValue();
-
-console.log(fluorValue);
-console.log(labraValue);
-console.log(opalValue);
-console.log(rubyValue);
-
-
-//reset game
-function resetGame() {
     fluorValue = generateCrystalValue();
     labraValue = generateCrystalValue();
     opalValue = generateCrystalValue();
     rubyValue = generateCrystalValue();
+
+    //functions-----------
+
+    //  generate random number
+    function generateTargetNum() {
+        var result = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        numTarget = result;
+        targetNum.text(result);
+    }
     generateTargetNum();
-    scoreUser = 0;
-};
 
-//increment win or loss
-if (scoreUser === numTarget) {
-    winsTally++;
-    resetGame();
-    alert("You Won!! Good Job!");
-} else if (scoreUser > numTarget) {
-    lossesTally++;
-    resetGame();
-    alert("You Lost! Boo Hoo!");
-};
+    //display random number on screen
+    targetNum.text(numTarget);
 
+    // assign random values to crystals
+    function generateCrystalValue() {
+        return Math.floor(Math.random() * (12 - 1)) + 1;
 
-//display win or loss
-wins.text(winsTally);
-losses.text(lossesTally);
-//display alert for win or loss
+    };
+    console.log(fluorValue);
+    console.log(labraValue);
+    console.log(opalValue);
+    console.log(rubyValue);
 
+    //reset game
+    function resetGame() {
+        scoreUser;
+        fluorValue = generateCrystalValue();
+        labraValue = generateCrystalValue();
+        opalValue = generateCrystalValue();
+        rubyValue = generateCrystalValue();
+        generateTargetNum();
+        console.log(fluorValue);
+        console.log(labraValue);
+        console.log(opalValue);
+        console.log(rubyValue);
+    };
 
-//add gem to score and display
-function tallyGems(x) {
-    scoreUser += x;
-    userScore.text(scoreUser);
-}
-// when click crystals add to score
-fluor.on("click", function () {
-    tallyGems(fluorValue);
-});
+    function tallyGems(x) {
+        scoreUser += x;
+        userScore.text(scoreUser);
+    };
 
-labra.on("click", function () {
-    tallyGems(labraValue);
-});
+    //increment win or loss
+    function compareScore() {
+        if (scoreUser === numTarget) {
+            winsTally++;
+            wins.text(winsTally);
+            resetGame();
+            alert("You Won!! Good Job!");
+            scoreUser = 0;
+        } else if (scoreUser > numTarget) {
+            lossesTally++;
+            losses.text(lossesTally);
+            resetGame();
+            alert("You Lost! Boo Hoo!");
+            scoreUser = 0;
+        };
+    };
 
-opal.on("click", function () {
-    tallyGems(opalValue);
-});
-
-ruby.on("click", function () {
-    tallyGems(rubyValue);
-});
-console.log(scoreUser);
-
-function gamePlay() {
-    resetGame();
+    //Game---------------------------------------------------
+    //add gem to score and display
+    // when click crystals add to score
     fluor.on("click", function () {
         tallyGems(fluorValue);
+        compareScore();
     });
 
     labra.on("click", function () {
         tallyGems(labraValue);
+        compareScore();
     });
 
     opal.on("click", function () {
         tallyGems(opalValue);
+        compareScore();
     });
 
     ruby.on("click", function () {
         tallyGems(rubyValue);
+        compareScore();
     });
-};
-gamePlay();
+});
